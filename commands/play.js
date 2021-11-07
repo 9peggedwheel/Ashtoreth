707d4705339435d05b20b519a13a4b4a7b162245
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-06 21:25:02 -0700

    fix subscribe

--- commands/play.js
+++ commands/play.js
@@ -103,7 +103,7 @@ const video_player = async (guild, song) => {
 	const resource = createAudioResource(stream); 
     // resource.volume.setVolume(0.5);
 	player.play(resource);
-    connection.subscribe(player);
+    song_quene.connection.subscribe(player);
     // song_queue.connection.playFile(stream, { seek: 0, volume: 0.5 })
     // .on('finish', () => {
     //     song_queue.songs.shift();
