2e5149ce7695a55cdde0efecee2c2647a1e3e199
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-09 20:09:40 -0800

    idiot proof

--- src/commands/admin/ban.js
+++ src/commands/admin/ban.js
@@ -5,6 +5,7 @@ module.exports.run = async (client, message, args) => {
         const member = message.mentions.users.first();
         if (member) {
             const memberTarget = message.guild.members.cache.get(member.id);
+            if (member.id === '905616259645845604') return message.reply("Nice try buddy");
             message.channel.send(`<@${memberTarget.user.id}> has been banned`);
             memberTarget.ban();
         } else {
--- src/commands/admin/mute.js
+++ src/commands/admin/mute.js
@@ -10,6 +10,7 @@ module.exports.run = async (client, message, args) => {
 
         let memberTarget = message.guild.members.cache.get(member.id);
         
+        if (member.id === '905616259645845604') return message.reply("Nice try buddy");
         if (!args[1]) {
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
