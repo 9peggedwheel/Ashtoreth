6653f04a9a600068ffef4c363e4a85a3ac2b0476
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-11 22:53:18 -0800

    ban/kick idiotproof

--- src/commands/admin/kick.js
+++ src/commands/admin/kick.js
@@ -6,11 +6,16 @@ module.exports.run = async (client, message, args) => {
         if (member) {
             const memberTarget = message.guild.members.cache.get(member.id);
             if (member.id === '905616259645845604') return message.reply("Nice try buddy");
-            memberTarget.kick().catch(error => {
+            if (message.member.highestRole.comparePositionTo(member.highestRole) > 0){
+                memberTarget.kick().catch(error => {
+                    message.channel.send("Calm down buddy");
+                    return;
+                });
+                message.channel.send(`<@${memberTarget.user.id}> has been kicked`);
+            } else {
                 message.channel.send("Calm down buddy");
                 return;
-            });
-            message.channel.send(`<@${memberTarget.user.id}> has been kicked`);
+            }
 
         } else {
             message.channel.send("Invalid member");
