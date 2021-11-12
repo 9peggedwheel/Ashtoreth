5d5e1a48cd0e21797e4d3aa40ad7aff92d9953f2
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-11 21:12:32 -0800

    debug welcome

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -6,7 +6,7 @@ module.exports = async (member) => {
         GuildID: member.guild
     });
 
-    let channel = member.guild.channels.cache.find(data.ChannelID);
+    let channel = data.ChannelID;
     let welcomeRole = member.guild.roles.cache.find(data.RoleID);
 
     member.roles.add(welcomeRole);
