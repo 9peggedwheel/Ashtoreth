57aa30afacd166809f8abf8c64bebeb7588653d7
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-11 21:36:50 -0800

    debug welcome

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const welcomeModel = require('../models/welcomeModel');
 
-module.exports = async (member) => {
+module.exports = async (client, member) => {
     const data = await welcomeModel.findOne({
         GuildID: member.guild
     });
 
-    let channel = member.guild.channels.cache.get(data.ChannelID);
+    let channel = client.channels.cache.get(data.ChannelID);
     let welcomeRole = member.guild.roles.cache.find(data.RoleID);
 
     member.roles.add(welcomeRole);
-    client.channels.cache.get(channel).send(`Hello <@${member.user.id}>, welcome to the server!`)
-    client.channels.cache.get(channel).send('https://media.discordapp.net/attachments/908229025099567146/908571572607008788/a.jpg')
+    client.channels.cache.get(channel.id).send(`Hello <@${member.user.id}>, welcome to the server!`)
+    client.channels.cache.get(channel.id).send('https://media.discordapp.net/attachments/908229025099567146/908571572607008788/a.jpg')
 }
