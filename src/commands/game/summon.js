27cbda15545f24dde522c306ba3f75a1f297102d
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-12 14:17:02 -0800

    debug

--- src/commands/game/summon.js
+++ src/commands/game/summon.js
@@ -49,7 +49,7 @@ module.exports.run = async (client, message, args) => {
         },
         {
             $addToSet: {
-                Inventory: `${character}`,
+                Inventory: character,
             }
         }
     );
