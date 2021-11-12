3bb72113a7911f9247db4f79be9a79fa2768a5cb
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-11 22:28:03 -0800

    idiotproof

--- src/commands/admin/kick.js
+++ src/commands/admin/kick.js
@@ -6,13 +6,10 @@ module.exports.run = async (client, message, args) => {
         if (member) {
             const memberTarget = message.guild.members.cache.get(member.id);
             if (member.id === '905616259645845604') return message.reply("Nice try buddy");
-            try {
-                memberTarget.kick();
-            } catch (e) {
-                console.log(e);
+            memberTarget.kick().catch(error => {
                 message.channel.send("Calm down buddy");
                 return;
-            }
+            });
             message.channel.send(`<@${memberTarget.user.id}> has been kicked`);
 
         } else {
