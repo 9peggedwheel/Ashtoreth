b25bc4f862b9c16e36b1f4747ed1ffcf8bbfa7fe
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2021-11-20 22:46:41 -0800

    fix kick

--- src/commands/admin/kick.js
+++ src/commands/admin/kick.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
     if (message.member.permissions.has("KICK_MEMBERS")) {
-        const member = message.mentions.users.first();
+        const member = message.mentions.members.first();
         if (member) {
             const memberTarget = message.guild.members.cache.get(member.id);
             if (member.id === '905616259645845604') return message.reply("Nice try buddy");
