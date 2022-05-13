0d87e0337565ba55373fbe3e8061d7a114c70b2e
YanzheF <93801835+9peggedwheel@users.noreply.github.com>
2022-05-12 18:50:36 -0700

    welcome database

--- guildMemberAdd.js
+++ guildMemberAdd.js
@@ -1,9 +1,19 @@
-module.exports = (client) => {
-    const channelId = "905960857601736736";
-    let welcomeRole = member.guild.roles.cache.find(role => role.name === 'Member');
+const mongoose = require('mongoose');
+const welcomeModel = require('../../models/welcomeModel');
 
-    member.roles.add(welcomeRole);
-    const channel = member.guild.channels.cache.get(channelId);
-    channel.send(`Welcome <@${member.id}> to the server!`);
-    channel.send('https://media.discordapp.net/attachments/906406839736999957/906417653344784384/BJQMzfn.png');
+module.exports = (client) => {
+    const serverID = await welcomeModel.findOne({
+        GuildID: member.guild.id
+    });
+
+    if (serverID) {
+            
+        const channelId = serverID.ChannelID;
+        let welcomeRole = member.guild.roles.cache.find(role => role.name === serverID.RoleID);
+
+        member.roles.add(welcomeRole);
+        const channel = member.guild.channels.cache.get(channelId);
+        channel.send(`Welcome <@${member.id}> to the server!`);
+        channel.send('https://media.discordapp.net/attachments/906406839736999957/906417653344784384/BJQMzfn.png');
+    }
 }
